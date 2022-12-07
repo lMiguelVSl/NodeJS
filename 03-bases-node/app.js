82153cc42f0c6d7acb10c17c559c9dbd39f482bc
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2022-12-07 10:24:37 -0500

    Managing the function as a promise and getting just the fileName

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -2,4 +2,4 @@ const { createFile } = require('./helpers/multiplication');
 
 console.clear();
 
-createFile(3);
+createFile(9).then(fileName => console.log(fileName)).catch(err => console.log(err));
--- 03-bases-node/helpers/multiplication.js
+++ 03-bases-node/helpers/multiplication.js
@@ -1,22 +1,22 @@
 const fs = require('fs');
 
 const createFile = (base) => {
-    console.log('===============');
-    console.log(`Number ${base} table`);
-    console.log('===============');
+    return new Promise((resolve, reject) => {
 
-    let salida = '';
+        let salida = '';
 
-    for (let i = 1; i <= 10; i++) {
-        i == 10 ? salida += `${base} x ${i} = ${base * i}` : salida += `${base} x ${i} = ${base * i}\n`;
-    }
+        for (let i = 1; i <= 10; i++) {
+            i == 10 ? salida += `${base} x ${i} = ${base * i}` : salida += `${base} x ${i} = ${base * i}\n`;
+        }
 
-    fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
-        if (err) throw err;
-        console.log('FILE CREATED');
+        try {
+            const fileName = `tabla-${base}.txt`;
+            fs.writeFileSync(fileName, salida);
+            resolve(fileName);
+        } catch (error) {
+            reject(error);
+        }
     });
-
-    console.log(salida);
 }
 
 module.exports = {
