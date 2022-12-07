ee29d6941226914bee2f21f294638b452d36d2c4
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2022-12-07 09:59:06 -0500

    creating a file saving the information generate with the table dinamic for a number base

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,8 +1,20 @@
+const fs = require('fs');
+
 console.clear();
 console.log('===============');
 console.log('Number 5 table');
 console.log('===============');
 
+const base = 5;
+let salida = '';
+
 for (let i = 1; i <= 10; i++) {
-    console.log(`5 x ${i} = ${5 * i}`)
+    i == 10 ? salida += `${base} x ${i} = ${base * i}` : salida += `${base} x ${i} = ${base * i}\n`;
 }
+
+fs.writeFile(`tabla-${base}.txt`, salida, (err) => {
+    if (err) throw err;
+    console.log('FILE CREATED');
+});
+
+console.log(salida);
