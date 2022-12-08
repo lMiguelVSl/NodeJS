e229e89a24b4aa2d1423d8d8419a5ab0c0bee4da
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2022-12-08 09:36:45 -0500

    Adding packages and managing versions

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -2,4 +2,7 @@ const { createFile } = require('./helpers/multiplication');
 
 console.clear();
 
-createFile(9).then(fileName => console.log(fileName)).catch(err => console.log(err));
+const [,,arg3 = 'base=5' ] = process.argv;
+const [, base ] = arg3.split('=');
+
+createFile(base).then(fileName => console.log(fileName)).catch(err => console.log(err));
