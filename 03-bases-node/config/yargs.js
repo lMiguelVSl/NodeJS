ea6bf1a50a1f07faf8c1c3a9fd42e40d6aca021d
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2022-12-11 10:41:09 -0500

    Adding description to the console options

--- 03-bases-node/config/yargs.js
+++ 03-bases-node/config/yargs.js
@@ -2,19 +2,21 @@ const argv = require('yargs')
     .options('b', {
         alias: 'base',
         type: 'number',
-        demandOption: true
+        demandOption: true,
+        describe: 'Number to get the multiplication table'
+    })
+    .options('l', {
+        alias: 'list',
+        type: 'boolean',
+        default: false,
+        describe: 'Show or not the result in console'
     })
     .check((argv, options) => {
         if (isNaN(argv.b)) throw 'The base have to be number type';
         return true;
     })
-    .options('l', {
-        alias: 'list',
-        type: 'boolean',
-        demandOption: false,
-        default: false
-    }).argv;
+    .argv;
 
-    module.exports = {
-        argv
-    }
+module.exports = {
+    argv
+}
