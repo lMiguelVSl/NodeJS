534ac502406d77fe771cae2f446a1441f0854610
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-01-02 16:24:47 -0500

    Create Task method

--- 04-task-list/models/Tasks.js
+++ 04-task-list/models/Tasks.js
@@ -1,9 +1,15 @@
+import { Task } from './task.js';
 
 class Tasks {
-    _list ={}
+    _list = {}
 
     constructor() {
         this._list = {}
+    }
+
+    createTask(desc = '') {
+        const task = new Task(desc);
+        this._list[task.Id] = task;
     }
 }
 
