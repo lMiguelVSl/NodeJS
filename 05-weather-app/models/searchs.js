5cc09a67e621574281932796bea8f633eee5c8ee
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-04 14:32:42 -0500

    Using nodemon and using axios instances for peticions

--- 05-weather-app/models/searchs.js
+++ 05-weather-app/models/searchs.js
@@ -7,13 +7,22 @@ export class Searchs {
     }
 
     async searchCity(place = '') {
-        try {
-            const res = await axios.get('https:reqres.in/api/users?page=2');
-            console.log(res.data);
-            return [];
 
-        } catch (error) {
-            return [];
-        }
+        const instance = axios.create({
+            baseURL: `https://api.mapbox.com/geocoding/v5/mapbox.places/${place}.json`,
+            params: {
+                'access_token': 'pk.eyJ1IjoibWlndWVsdnNsIiwiYSI6ImNsZGY4dmRqcDBnZmIzcW1wenNweHo3bGsifQ.Byy9BY57lcBcmdQ48kUN_w',
+                'limit': 5,
+                'languaje': 'es'
+            }
+        });
+
+        await instance.get()
+            .then(res => {
+                console.log('DATA', res.data);
+                return;
+            })
+            .catch(err => console.log('error axios get'));
+
     }
 }
