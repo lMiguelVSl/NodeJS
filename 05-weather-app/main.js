9a1f60bb0c5c124999e2964ce4ea54fe87f160bf
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-05 10:45:43 -0500

    Basic place information in console

--- 05-weather-app/main.js
+++ 05-weather-app/main.js
@@ -1,4 +1,4 @@
-import { inquirerMenu, pause, readInput } from "./helpers/inquirer.js";
+import { inquirerMenu, pause, placesList, readInput } from "./helpers/inquirer.js";
 import { Searchs } from "./models/searchs.js";
 import "dotenv/config.js";
 
@@ -9,12 +9,20 @@ import "dotenv/config.js";
         opt = await inquirerMenu();
         switch (opt) {
             case 1:
-                let text = await readInput('City to search: ');
-                await searchs.searchCity(text);
+                //show message
+                let userInput = await readInput('City to search: ');
+                //search places
+                let places = await searchs.searchCity(userInput);
+                let id = await placesList(places);
+                //selected place
+                const { name, lat, lng } = places.find(m => m.id === id);
+                //weather
+
+                //show results
                 console.log('\n City Information: \n'.green);
-                console.log('City:',);
-                console.log('Lat:',);
-                console.log('Lng:',);
+                console.log('City:', name);
+                console.log('Lat:', lat);
+                console.log('Lng:', lng);
                 console.log('Temp:',);
                 console.log('Temp Min:',);
                 console.log('Temp Max',);
