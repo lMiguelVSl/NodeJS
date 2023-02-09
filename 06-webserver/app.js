000cae0aebc9340973b5623b827983e437854a40
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-02-08 22:23:33 -0500

    Using Handlebars to render content

--- 06-webserver/app.js
+++ 06-webserver/app.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 const port = 3000;
 
+//handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+//static content
 app.use(express.static('public'));
 
-app.get('/hello-world', (req, res) => {
-    res.send('Hello from Express');
+app.get('/', (req, res) => {
+    res.render('home', {
+        name: 'Miguel Angel Vargas',
+        title: 'Node Course'
+    });
+})
+
+app.get('/index', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+})
+
+app.get('/generic', (req, res) => {
+    res.sendFile(__dirname + '/public/generic.html');
 })
 
 app.get('*', (req, res) => {
